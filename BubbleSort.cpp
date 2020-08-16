95b7cafb0b801057b5d6f9cf188dab7f0107a662
donghankwon <donghankwon@naver.com>
2020-08-16 14:01:29 +0900

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -3,8 +3,9 @@
 int main()
 {
 	int arr[1024][128]; //정리할 배열 
-	int swap, cmp; //바꿈, 비교 횟수 
+	int swap, cmp; //교환, 비교 횟수 
 	int n, m; //표본수, 항목수 
+	int temp; //교환인수  
 	
 	printf("Input Sample/Item Quantity (Max 128/1024) : ");
 	scanf("%d %d", &n, &m);
@@ -15,6 +16,23 @@ int main()
 			
 	for(int k=0; k<n; k++)
 	{
-		
+		swap=0; cmp=0;
+		for(int j=m-1; j>=1; j--)
+		{
+			for(int i=0; i<j; i++)
+			{
+				cmp++;
+				if(arr[i][k]>arr[i+1][k])
+				{
+					swap+=3;
+					temp=arr[i][k];
+					arr[i][k]=arr[i+1][k];
+					arr[i+1][k]=temp; 
+				}
+			}
+		}
+		printf("Swap : %d / Compare : %d\n", swap, cmp);
 	}
+	system("PAUSE");
+	return 0;
 }
